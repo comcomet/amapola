184dcded8ce1281db1ea7ab2766a3f7475abaf67
comcomet <52145180+comcomet@users.noreply.github.com>
2019-08-30 14:40:06 +0900

    Add files via upload

---
+++ amapola.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 11:48:58 2019
+
+@author: amapola
+"""
+
+#%%
+a=1
+bread = '뻥'
+print('gogo') 
+#%%
+print(bread)
+print('1')
